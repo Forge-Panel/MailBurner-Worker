d40a2e2e366dac616ee5b45c7adcf609020743ee
Spider-Frog <germanspiderfrog@gmail.com>
2025-03-14 12:53:35 +0100

    Project setup

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { Queue, Worker } from 'bullmq';
+
+const myQueue = new Queue(
+  'foo',
+  {
+    connection: {
+      host: "127.0.0.1",
+      port: 6379,
+    }
+  }
+);
+
+async function addJobs() {
+  await myQueue.add('myJobName', { foo: 'bar' });
+  await myQueue.add('myJobName', { qux: 'baz' });
+}
+
+await addJobs();
+
+const worker = new Worker(
+  'foo',
+  async job => {
+    // Will print { foo: 'bar'} for the first job
+    // and { qux: 'baz' } for the second.
+    console.log("i will run a job");
+    console.log(job.data);
+  },
+  {
+    connection: {
+      host: "127.0.0.1",
+      port: 6379,
+    }
+  },
+);
